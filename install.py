fd5cc4fc455a44ae5418a4120b7f899eb5a44a8f
Diogo Fernandes <dfop02@hotmail.com>
2018-12-03 12:18:57 -0200

    Suporte para Mac

--- install.py
+++ install.py
@@ -6,11 +6,11 @@ from shutil import copy
 
 # Principal arquivo do seu programa
 main = "menu.py"
-# Pacotes necessario para seu programa funcionar
-pkg = ["string", "random", "pygame"]
+# Pacotes necessario para o instalador funcionar
+deps = ["pipreqs"]
 # Nome do seu programa (Nome do atalho na area de trabalho)
 name = "Jogo da Forca"
-# Comentario no atalho (Opicional)
+# Comentario no atalho (Opicional, apenas para Linux)
 comment = "Jogo da Forca"
 # Imagem do icone do jogo precisa estar em formado .png e na pasta img
 icon = "icon.png"
@@ -28,6 +28,7 @@ def getPath():
 	return os.path.dirname(os.path.abspath(__file__))
 
 def checkUser():
+	system = getSystem()
 	# Forca diretorio
 	forcaDir = getPath()
 	# Desktop diretorio
@@ -35,10 +36,18 @@ def checkUser():
 	if not os.path.isdir(root):
 		root = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
 
-	routesPath = forcaDir.split('/')
-	routesRoot = root.split('/')
+	if system == "Windows":
+		routesPath = forcaDir.split('//')
+		routesRoot = root.split('//')
+	else:
+		routesPath = forcaDir.split('/')
+		routesRoot = root.split('/')
+
 	if routesPath[2] != routesRoot[2]:
-		realRoot = "/" + routesRoot[1] + "/" + routesPath[2] + "/" + routesRoot[3]
+		if system == "Windows":
+			realRoot = "//" + routesRoot[1] + "//" + routesPath[2] + "//" + routesRoot[3]
+		else:
+			realRoot = "/" + routesRoot[1] + "/" + routesPath[2] + "/" + routesRoot[3]
 		return realRoot
 	else:
 		return root
@@ -46,19 +55,34 @@ def checkUser():
 def setPythonFile():
 	python2Exe = "#!/usr/bin/env python\n"
 	python3Exe = "#!/usr/bin/env python3\n"
+	macPython2 = "#!/usr/local/bin/python"
+	macPython3 = "#!/usr/local/bin/python3"
+	system = getSystem()
 	exe = True
 	with open(main, "r+") as py:
 		firstLine = py.readlines()
-		for lines in firstLine:		
-			if not python2Exe == lines or python3Exe == lines:
-				exe = False
-				if getPythonVersion() >= (3,0):
-					firstLine.insert(0,python3Exe)
+		for lines in firstLine:
+			if system == 'Linux':
+				if not python2Exe == lines or python3Exe == lines:
+					exe = False
+					if getPythonVersion() >= (3,0):
+						firstLine.insert(0,python3Exe)
+					else:
+						firstLine.insert(0,python2Exe)
+					break
 				else:
-					firstLine.insert(0,python2Exe)
-				break
-			else:
-				break
+					break
+			elif system == 'Darwin':
+				if not macPython2 == lines or macPython3 == lines:
+					exe = False
+					if getPythonVersion() >= (3,0):
+						firstLine.insert(0,macPython3)
+					else:
+						firstLine.insert(0,macPython2)
+					break
+				else:
+					break
+
 	if not exe:
 		with open(main, "w") as py:
 			print ("Adicionando comando ao arquivo .py")
@@ -84,8 +108,22 @@ def getPipVersion():
 			vers += x + '.'
 	return float(vers[:-1])
 
+def searchPkg(path):
+	if os.path.isfile(path + "/requirements.txt"):
+		os.system("pipreqs --force" + path)
+	else:
+		os.system("pipreqs " + path)
+
+def installPkg():
+	system = getSystem()
+	if system == 'Linux':
+		os.system("sudo pip3 install -r requirements.txt")
+	else:
+		os.system("pip3 install -r requirements.txt")
+
 def install(package):
 	import importlib
+	system = getSystem()
 	try:
 		importlib.import_module(package)
 		print (package + " ja instalado.")
@@ -97,17 +135,31 @@ def install(package):
 			if (getPipVersion() < 18.0):
 				print ("Seu pip esta desatualizado...")
 				print ("Atualizando seu pip do " + pip.__version__ + " para o 18.0")
-				if getPythonVersion() >= (3,0):
-					os.system('sudo pip3 install -U pip')
-					os.system('sudo pip3 install ' + package)
-				else:
-					os.system('sudo pip install -U pip')
-					os.system('sudo pip install ' + package)
+				if system == 'Linux':
+					if getPythonVersion() >= (3,0):
+						os.system('sudo pip3 install -U pip')
+						os.system('sudo pip3 install ' + package)
+					else:
+						os.system('sudo pip install -U pip')
+						os.system('sudo pip install ' + package)
+				elif system == 'Darwin' or system == 'Windows':
+					if getPythonVersion() >= (3,0):
+						os.system('pip3 install -U pip')
+						os.system('pip3 install ' + package)
+					else:
+						os.system('pip install -U pip')
+						os.system('pip install ' + package)					
 			else:
-				if getPythonVersion() >= (3,0):
-					os.system('sudo pip3 install ' + package)
-				else:
-					os.system('sudo pip install ' + package)
+				if system == 'Linux':
+					if getPythonVersion() >= (3,0):
+						os.system('sudo pip3 install ' + package)
+					else:
+						os.system('sudo pip install ' + package)
+				elif system == 'Darwin' or system == 'Windows':
+					if getPythonVersion() >= (3,0):
+						os.system('pip3 install ' + package)
+					else:
+						os.system('pip install ' + package)
 			importlib.reload(pip)
 			print (getPipVersion())
 			if (getPipVersion() < 18.0):
@@ -116,11 +168,18 @@ def install(package):
 		except ImportError:
 			try:
 				print ("Pip nao instalado... Instalando...")
-				# Instala o pip e depois atualiza para o mais recente
-				if getPythonVersion() >= (3,0):
-					os.system('sudo apt-get install python3-pip && sudo pip3 install -U pip')
-				else:
-					os.system('sudo apt-get install python-pip && sudo pip install -U pip')
+				if system == 'Linux':
+					if getPythonVersion() >= (3,0):
+						os.system('sudo apt-get install python3-pip && sudo pip3 install -U pip')
+					else:
+						os.system('sudo apt-get install python-pip && sudo pip install -U pip')
+				elif system == 'Windows':
+					print('Not implemented yet.')
+				elif system == 'Darwin':
+					if getPythonVersion() >= (3,0):
+						os.system('brew install python3 && pip3 install -U pip')
+					else:
+						os.system('brew install python && pip install -U pip')
 			except IOError as error:
 				print (error)
 				print ("Erro ao instalar o pip... Instalacao abortada.")
@@ -128,42 +187,68 @@ def install(package):
 			finally:
 				import pip
 				print ("Tentando novamente... Instalando " + package + "...")
-				if getPythonVersion() >= (3,0):
-					os.system('sudo pip3 install ' + package)
-				else:
-					os.system('sudo pip install ' + package)
+				if system == 'Linux':
+					if getPythonVersion() >= (3,0):
+						os.system('sudo pip3 install ' + package)
+					else:
+						os.system('sudo pip install ' + package)
+				elif system == 'Darwin' or system == 'Windows':
+					if getPythonVersion() >= (3,0):
+						os.system('pip3 install ' + package)
+					else:
+						os.system('pip install ' + package)
 
 def installDeps():
 	# As Deps sao declaradas la no comeco
 	print ("Procurando pacotes pendentes...")
-	for lib in pkg:
+	for lib in deps:
 		install(lib)
+
+def getSystem():
+	import platform
+	return platform.system()
 
 def createShortcut(path, version, currentRoot):
 	os.chdir(currentRoot)
-	with open(name + ".desktop", "w") as shortcut:
-		shortcut.write("[Desktop Entry]\n")
-		shortcut.write("Version=" + version + "\n")
-		shortcut.write("Name=" + name + "\n")
-		shortcut.write("Comment=" + comment + "\n")
-		shortcut.write("Exec=" + path + "/" + main + "\n")
-		shortcut.write("Icon=" + path + "/img/" + icon + "\n")
-		shortcut.write("Type=Application\n")
-		shortcut.write("Terminal=false")
-	try:
-		copy(getPath() + "/" + name + ".desktop", os.path.expanduser('~/.local/share/applications'))
-	except IOError as error:
-		print (error)
-		print ("Erro de permissao... Instalacao abortada.")
-		exit()
-	turnRun(name + ".desktop")
+	system = getSystem()
+
+	if system == 'Linux':
+		with open(name + ".desktop", "w") as shortcut:
+			shortcut.write("[Desktop Entry]\n")
+			shortcut.write("Version=" + version + "\n")
+			shortcut.write("Name=" + name + "\n")
+			shortcut.write("Comment=" + comment + "\n")
+			shortcut.write("Exec=" + path + "/" + main + "\n")
+			shortcut.write("Icon=" + path + "/img/" + icon + "\n")
+			shortcut.write("Type=Application\n")
+			shortcut.write("Terminal=false")
+		try:
+			copy(getPath() + "/" + name + ".desktop", os.path.expanduser('~/.local/share/applications'))
+		except IOError as error:
+			print (error)
+			print ("Erro de permissao... Instalacao abortada.")
+			exit()
+		turnRun(name + ".desktop")
+
+	elif system == 'Darwin':		
+		createMacApp(path)		
+		turnRun(name + ".command")
+
+	elif system == 'Windows':
+		print('System desktop file not implemented yet.')
+
+def createMacApp(path):
+	with open(name + ".command", "w") as shortcut:
+		shortcut.write("#!/bin/sh\npython3 " + path + "/" + main)
 
 if __name__ == '__main__':
 	# Corrige o path caso tente instalar por outro usuario...
 	currentRoot = checkUser()
-
-	print ("Instalando Jogo da Forca...")
+	print ("Instalando dependencias do instalador...")
 	installDeps()
+	print ("Instalando " + name + "...")
+	searchPkg(getPath())
+	installPkg()
 
 	print ("Tornando o arquivo executavel...")
 	setPythonFile()
@@ -178,5 +263,4 @@ if __name__ == '__main__':
 		print ("Erro de permissao... Instalacao abortada.")
 		exit()
 	print ("Arquivo criado com sucesso!")
-
 	print ("Instalacao concluida!")
